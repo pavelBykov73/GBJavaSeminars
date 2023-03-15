62076f1f7d6dd982347ac247a6b3d6173a6b2e5b
PavelB <pavel.bykov73@yandex.ru>
2023-03-15 15:34:41 +0300

    Add Ex1

---
+++ seminar4/src/main/java/org/example/hw1/Ex1.java
@@ -0,0 +1,30 @@
+package org.example.hw1;
+
+import java.util.LinkedList;
+import java.util.Random;
+
+/**
+ * Вывести список на экран в перевернутом виде (без массивов и ArrayList)
+ * Пример:
+ * 1 -> 2->3->4
+ * Вывод:
+ * 4->3->2->1
+ */
+public class Ex1 {
+    public static void main(String[] args) {
+        LinkedList<Integer> numberList = new LinkedList<>();
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            numberList.add(i, random.nextInt(20));
+        }
+        System.out.println(numberList);
+        System.out.println(reverseList(numberList));
+    }
+
+    private static LinkedList<Integer> reverseList(LinkedList<Integer> list) {
+        for (int i = 0, j = list.size() - 1; i < j; i++) {
+            list.add(i, list.remove(j));
+        }
+        return list;
+    }
+}
