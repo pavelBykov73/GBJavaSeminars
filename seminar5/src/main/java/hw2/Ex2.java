0799028456092d7403b04625fdebed615e91693b
PavelB <pavel.bykov73@yandex.ru>
2023-03-20 09:23:22 +0300

    modify code of Ex2

--- seminar5/src/main/java/hw2/Ex2.java
+++ seminar5/src/main/java/hw2/Ex2.java
@@ -8,39 +8,37 @@ import java.util.*;
  * (наполнением значение, где искомое слово будет являться ключом), вносить все слова не нужно
  */
 public class Ex2 {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите имя файла:");
+        System.out.println("Введите имя файла для поиска слова:");
         String fileName = scanner.nextLine();
-        System.out.println("Что ищем? Введите слово:");
-        String wordToSearch = scanner.nextLine();
-        System.out.println("Количество вхождений слова = " + wordCountInFile(fileName, wordToSearch));
+        File file = new File(fileName);
+        if (isFileExists(file)) {
+            System.out.println("Что ищем? Введите слово:");
+            String wordToSearch = scanner.next();
+            System.out.printf("Количество вхождений слова \"%s\" = %d",
+                    wordToSearch, wordCountInFile(file, wordToSearch));
+        } else {
+            System.out.println("Файл не существует!");
+        }
     }
 
     /**
      * Поиск и подсчет количества слов в файле
      *
-     * @param filePathName
+     * @param file
      * @param wordToSearch
      * @return число вхождений
      */
-    public static int wordCountInFile(String filePathName, String wordToSearch) {
+    public static int wordCountInFile(File file, String wordToSearch) {
         int wordCount = 0;
         String inStr;
-        File file = new File(filePathName);
         try {
-            if (isFileExists(file)) {
-                FileReader reader = new FileReader(file);
-                BufferedReader bufferedReader = new BufferedReader(reader);
-                while ((inStr = bufferedReader.readLine()) != null) {
-                    wordCount += wordCountInString(inStr, wordToSearch);
-                }
-            } else {
-                System.out.println("Файл не найден: " + filePathName);
+            FileReader reader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            while ((inStr = bufferedReader.readLine()) != null) {
+                wordCount += wordCountInString(inStr, wordToSearch);
             }
-//        } catch (FileNotFoundException f) {
-//            System.out.println("Ошибка. Файл не найден.");
         } catch (IOException e) {
             System.out.println("Ошибка чтения файла.");
         }
@@ -61,6 +59,7 @@ public class Ex2 {
      * Поиск и подсчет количества слов в строке без учетов знаков припинания
      *
      * @param s - строка
+     * @return число вхождений
      */
     private static int wordCountInString(String s, String searchWord) {
         Map<String, Integer> map = new HashMap<>();
@@ -69,7 +68,7 @@ public class Ex2 {
         // варианты регулярки:
         // [—!\"#$%&'()*+,-./:;<=>?@\\[\\]^_`{|}~–]
         // \\p{IsPunctuation}
-        String str = s.replaceAll("\\p{IsPunctuation}", "");
+        String str = s.replaceAll("\\p{IsPunctuation}", " ");
         String[] words = str.split("\\s+");
         //System.out.println(str);
         for (String word : words
